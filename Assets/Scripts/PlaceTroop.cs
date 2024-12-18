cbd01d6bfb298e1c7abf421b1f3492e9226ac112
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2024-12-18 00:58:48 -0300

    Add TMP e possibilita escolher tropa para colocar em jogo

--- Assets/Scripts/PlaceTroop.cs
+++ Assets/Scripts/PlaceTroop.cs
@@ -13,8 +13,15 @@ public class PlaceTroop : MonoBehaviour
     [SerializeField]
     private AgentManager agentManager;
 
+    private int currentTroop = -1;
+
     void Update()
     {
+        if (currentTroop == -1)
+        {
+            return;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -29,8 +36,26 @@ public class PlaceTroop : MonoBehaviour
             // Se o raycast acertar algo na layer do terreno
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrainLayer))
             {
-                agentManager.AddAlly(0, hit.point);
+                agentManager.AddAlly(currentTroop, hit.point);
             }
         }
+    }
+
+    public void setCurrentTroopMiniPekka()
+    {
+        currentTroop = 0;
+        Debug.Log("Set Current Troop - Mini Pekka");
+    }
+
+    public void setCurrentTroopSkeletonArmy()
+    {
+        currentTroop = 1;
+        Debug.Log("Set Current Troop - Skeleton Army");
+    }
+
+    public void setCurrentTroopGiant()
+    {
+        currentTroop = 2;
+        Debug.Log("Set Current Troop - Giant");
     }
 }
